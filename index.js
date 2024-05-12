989bd3e3c952054beaad6a5c2acd80f61db8bfcb
Tom Bangle <tombangle@gmail.com>
2024-05-12 13:06:13 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch(`http://localhost:3000/users`, {
+        method: `POST`,
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const newID = document.createElement(`div`);
+        newID.textContent = `ID: ${data.id}`;
+        document.body.appendChild(newID);
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        const errorMessage = document.createElement(`div`);
+        errorMessage.textContent = error.message;
+        document.body.appendChild(errorMessage);
+    });
+}
